451b121d468fe14fb0b30f802501ab280f04b27f
vlasJedi <vlas.d78@gmail.com>
2019-02-15 00:32:55 +0200

    added some xhr

--- main.js
+++ main.js
@@ -23,7 +23,44 @@ requirejs(["modules/scripts/alertHello", "jquery"],function( alertHello, $ ){
 	$(document).ready( function () {
 		alert("Value from config" + alertHello.someConfig);
 		alertHello.sayHelloJquery();
-		xhr = new XMLHttpRequest();
+		var xhr = new XMLHttpRequest();
+		//xhr.withCredentials = true;
+		var request = {
+			method: "GET",
+			url: "https://my-json-server.typicode.com/vlasJedi/RestAPI", //Same Origin Policy - by default only possible access to prot://domain:port
+			asynch: true,
+			user: null,
+			password: null
+		}
+		xhr.open(request.method,request.url,request.asynch,request.user,request.password);  // initializates request
+		xhr.setRequestHeader("Access-Control-Allow-Origin","file:///C:/requireJS");
+		xhr.send(); // is async by default, but can be sync  -- as argument can take next
+		//xhr.status; // code
+		//xhr.statusText; // text to code
+		//xhr.responseText; // response text from server
+		//xhr.responseXML; // response in XMl
+		//xhr.abort;
+		//var query = {
+			//fname: "vlas",
+			//sname: "dielov"
+		//};
+		//var URLqueryObj = new URLSearchParams();
+		//for( var key in query ) {
+			//URLqueryObj.set( key , query[key] );
+		//}
+		//console.log(URLqueryObj.toString());
+		//xhr.send(URLqueryObj);
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState != 4 ) {
+				console.log("Current readyState value is: " + xhr.readyState );
+				return;
+			}
+			console.log("Server status code xhr.status: " + xhr.status + ", server status text: " + xhr.statusText );
+			console.log("Response from server xhr.responseText: " + xhr.responseText);
+		};
+		
+		
+		
 		
 	})
 } );
